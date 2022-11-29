b73d8b3a5344ad70dbbc770304ea6a06ebb73dd4
Martin Scholz <martin.scholz@se.com>
2022-11-29 22:10:32 +0100

    added ppo

--- sb_lunar_Lander.py
+++ sb_lunar_Lander.py
@@ -1,23 +1,73 @@
 import gym
-import sys
+import sys, os
 
-from stable_baselines3 import DQN
+from stable_baselines3 import DQN, PPO
 from stable_baselines3.common.evaluation import evaluate_policy
+from stable_baselines3.common.callbacks import BaseCallback
 
 env_id = "LunarLander-v2"
 local_dir = f"sb_runs_{env_id}"
 
+#define checkpoint folder
+checkpoint_dir =  os.path.join(local_dir, f"./checkpoints/{env_id}")
+log_dir = os.path.join(local_dir, f"./log/{env_id}")
+
 # Create environment
 env = gym.make(env_id)
 
-def train():
+mode = "PPO"
+timesteps = 2_000_000
+
+
+class TrainAndLoggingCallback(BaseCallback):
+
+    def __init__(self, check_freq, save_path, verbose=1):
+        super(TrainAndLoggingCallback, self).__init__(verbose)
+        self.check_freq = check_freq
+        self.save_path = save_path
+
+    def _init_callback(self):
+        if self.save_path is not None:
+            os.makedirs(self.save_path, exist_ok=True)
+
+    def _on_step(self):
+        if self.n_calls % self.check_freq == 0:
+            model_path = os.path.join(self.save_path, 'checkpoint_{}'.format(self.n_calls))
+            self.model.save(model_path)
+
+        return True
+
+def train_dqn():
     # Instantiate the agent
-    model = DQN('MlpPolicy', env, verbose=1)
+    model = DQN('MlpPolicy', env, tensorboard_log=log_dir, verbose=1)
+
     # Train the agent
-    model.learn(total_timesteps=int(200000))
+    callback = TrainAndLoggingCallback(check_freq=10000, save_path=checkpoint_dir)
+    model.learn(total_timesteps=timesteps, callback=callback)
     # Save the agent
-    model.save("dqn_lunar")
+    folder = os.path.join(local_dir, "dqn_lunar")
+    model.save(folder)
     del model  # delete trained model to demonstrate loading
+
+def train_ppo():
+    # Instantiate the agent
+    model = PPO('MlpPolicy', env, tensorboard_log=log_dir, verbose=1)
+    
+    # Train the agent
+    callback = TrainAndLoggingCallback(check_freq=10000, save_path=checkpoint_dir)
+    model.learn(total_timesteps=timesteps, callback=callback)
+    # Save the agent
+    folder = os.path.join(local_dir, "ppo_lunar")
+    model.save(folder)
+    del model  # delete trained model to demonstrate loading
+
+def train():
+
+    if mode == "DQN":
+        train_dqn()
+    elif mode == "PPO":
+        train_ppo()
+
 
 
 def enjoy():
@@ -25,7 +75,12 @@ def enjoy():
     # NOTE: if you have loading issue, you can pass `print_system_info=True`
     # to compare the system on which the model was trained vs the current one
     # model = DQN.load("dqn_lunar", env=env, print_system_info=True)
-    model = DQN.load("dqn_lunar", env=env)
+    if mode == "DQN":
+        folder = os.path.join(local_dir, "dqn_lunar")
+        model = DQN.load(folder, env=env)
+    elif mode == "PPO":
+        folder = os.path.join(local_dir, "ppo_lunar")
+        model = PPO.load(folder, env=env)
 
     # Evaluate the agent
     # NOTE: If you use wrappers with your environment that modify rewards,
